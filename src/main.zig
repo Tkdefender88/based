71e25ade0952b2bef990bc732cd487b33a840765
Justin Bak (╯°□°)╯︵ ┻━┻ <me@justinbak.com>
2026-02-21 08:10:35 -0700

    batman

---
+++ src/main.zig
@@ -0,0 +1,46 @@
+const std = @import("std");
+const base64z = @import("base64z");
+
+const Base64 = struct {
+    _table: *const [64]u8,
+
+    pub fn init() Base64 {
+        const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const lower = "abcdefghijklmnopqrstuvwxyz";
+        const numbers_symb = "0123456789+/";
+        return Base64 {
+            ._table = upper ++ lower ++ numbers_symb,
+        };
+    }
+
+    pub fn char_at(self: Base64, index: usize) u8 {
+        return self._table[index];
+    }
+};
+
+pub fn main() !void {
+    const base64 = Base64.init();
+    std.debug.print(
+        "Character at index 28: {c}\n",
+        .{base64.char_at(28)}
+    );
+}
+
+test "simple test" {
+    const gpa = std.testing.allocator;
+    var list: std.ArrayList(i32) = .empty;
+    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
+    try list.append(gpa, 42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
+
+test "fuzz example" {
+    const Context = struct {
+        fn testOne(context: @This(), input: []const u8) anyerror!void {
+            _ = context;
+            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
+            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+        }
+    };
+    try std.testing.fuzz(Context{}, Context.testOne, .{});
+}
